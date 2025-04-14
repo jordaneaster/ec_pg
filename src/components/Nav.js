2a61ad4173995b487f488514ad5c3e2a8b16af57
jordaneaster <jordaneaster222@icloud.com>
2025-04-14 15:49:43 -0400

    update nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -92,9 +92,9 @@ export default function Nav() {
               <li><Link href="/events" onClick={() => setMenuOpen(false)}>Events</Link></li>
               <li><Link href="/gallery" onClick={() => setMenuOpen(false)}>Gallery</Link></li>
               <li><Link href="/shop" onClick={() => setMenuOpen(false)}>Shop</Link></li>
-              <li><Link href="/reservations" onClick={() => setMenuOpen(false)}>Attend</Link></li>
-              <li><Link href="/cart" onClick={() => setMenuOpen(false)}>Cart</Link></li>
-              {user ? (
+              {/* <li><Link href="/reservations" onClick={() => setMenuOpen(false)}>Attend</Link></li>
+              <li><Link href="/cart" onClick={() => setMenuOpen(false)}>Cart</Link></li> */}
+              {/* {user ? (
                 <>
                   <li><Link href="/account" onClick={() => setMenuOpen(false)}>Profile</Link></li>
                   <li><Link href="/orders" onClick={() => setMenuOpen(false)}>Orders</Link></li>
@@ -111,7 +111,7 @@ export default function Nav() {
                 </>
               ) : (
                 <li><Link href="/login" onClick={() => setMenuOpen(false)}>Members</Link></li>
-              )}
+              )} */}
             </ul>
           </div>
         </div>
